7bdcd37ea25e7c5cf2c10bbe73e38c1ce005c197
unknown <you@example.com>
2020-05-10 13:23:59 +0100

    Version 3

--- app/src/main/java/com/example/adhan_application/MainActivity.kt
+++ app/src/main/java/com/example/adhan_application/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.adhan_application
 
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
+import android.widget.TextView
 import android.widget.TimePicker
 import androidx.core.app.ActivityCompat
 
@@ -25,6 +32,53 @@ class MainActivity : AppCompatActivity() {
         timePicker.setOnTimeChangedListener(TimePicker.OnTimeChangedListener { timePicker, hour, minute ->
             ServiceCaller(intent)
         })
+
+        val listView=findViewById<ListView>(R.id.time_listView)
+        listView.adapter=MyCustomAdapter(this)
+
+    }
+    private class MyCustomAdapter(context:Context):BaseAdapter(){
+
+        private val mContext:Context
+
+        private val salatName= arrayListOf<String>(
+            "Sobh(Fajr)","Dohr","Asr","Maghrib","Isha"
+        )
+        private val timeSalat= arrayListOf<String>(
+            "04:10","12:42","16:31","19:39","21:16"
+        )
+
+        init {
+            this.mContext=context
+        }
+
+        override fun getCount(): Int {
+            return salatName.size
+        }
+
+        override fun getItemId(position: Int): Long {
+
+            return position.toLong()
+        }
+
+        override fun getItem(position: Int): Any {
+
+            return "STRING"
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+            val layoutInflater=LayoutInflater.from(mContext)
+            val rowTime=layoutInflater.inflate(R.layout.row_time,parent,false)
+
+           val salat= rowTime.findViewById<TextView>(R.id.textView)
+            val time=rowTime.findViewById<TextView>(R.id.textView2)
+
+            salat.text=salatName.get(position)
+            time.text=timeSalat.get(position)
+            return rowTime
+
+             }
 
     }
 
